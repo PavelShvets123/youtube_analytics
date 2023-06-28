541472ccb46ad836641d0a28aa269ace7797ca6a
Pavel <pasha.shvets99@mail.ru>
2023-06-28 21:23:06 +0300

    homework 2

--- src/channel.py
+++ src/channel.py
@@ -2,34 +2,31 @@ import json
 import os
 from googleapiclient.discovery import build
 
-from helper.youtube_api_manual import channel_id, api_key
-
-youtube = build('youtube', 'v3', developerKey=api_key)
+api_key: str = os.getenv('YT_API_KEY')
 
 class Channel:
     """Класс для ютуб-канала"""
-
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-
+        channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         self.channel_id = channel_id
-        # self.data_id = channel_id['items']['snipped']['title']
-        self.title = channel_id['items'][0]['snipped']['title']
-        # self.description = description
-        # self.subscriber_count = subscriber_count
-        # self.video_count = video_count
-        # self.view_count = view_count
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = channel['items'][0]['snippet']["thumbnails"]["default"]["url"]
+        self.subscribercount = channel['items'][0]["statistics"]['subscriberCount']
+        self.video_count = channel['items'][0]["statistics"]['videoCount']
+        self.viewcount = channel['items'][0]["statistics"]['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        dict_to_print = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        dict_to_print = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
     @classmethod
     def get_service(cls):
-        return build('youtube', 'v3', developerKey=api_key)
+        return cls.youtube
 
     def to_json(self, dict_to_print):
         json.dumps(dict_to_print, indent=2, ensure_ascii=False)
