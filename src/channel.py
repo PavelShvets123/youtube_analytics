f682032616b44f0ce1de1cb6b9f03053ba59e748
Pavel <pasha.shvets99@mail.ru>
2023-06-29 00:57:24 +0300

    homework 2 Вариант 4

--- src/channel.py
+++ src/channel.py
@@ -8,16 +8,20 @@ class Channel:
     """Класс для ютуб-канала"""
     youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, __channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        self.channel_id = channel_id
+        channel = self.youtube.channels().list(id=__channel_id, part='snippet,statistics').execute()
+        self.__channel_id = __channel_id
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
         self.url = f'https://youtu.be/{channel["etag"]}'
         self.subscriber_count = channel['items'][0]["statistics"]['subscriberCount']
         self.video_count = channel['items'][0]["statistics"]['videoCount']
         self.view_count = channel['items'][0]["statistics"]['viewCount']
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -38,3 +42,4 @@ class Channel:
                 "video_count": self.video_count,
                 "view_count": self.view_count
             }, indent=2, ensure_ascii=False))
+
